54e4d52c5f13c61beb71d05be12435c575a96aaf
JustisGuin <71509622+JustisGuin@users.noreply.github.com>
2024-12-04 21:54:55 -0500

    User input

---
+++ theStuff.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+int main(){
+    char c;
+
+    while(1){
+        printf("Please enter a char (Press X to exit program:\n");
+        scanf("%c", &c);
+
+
+        if (c =="X"){
+            break;
+        }
+    }
+
+    //Logic for translating c (char)
+    
+
+    return 0;
+}
