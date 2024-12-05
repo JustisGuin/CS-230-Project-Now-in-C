80e5336e2f2426da47d5fa9a910a35ae825a4c5f
JustisGuin <71509622+JustisGuin@users.noreply.github.com>
2024-12-05 14:39:30 -0500

    Translating Hex Digit and Bitwise

--- theStuff.c
+++ theStuff.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
-
+#include <ctype.h>
 
 
 int main(){
-    char c;
+    char hexDigit;
+    
+
 
     while(1){
-        printf("Please enter a char (Press X to exit program:\n");
-        scanf("%c", &c);
+        printf("Please enter a hexadecimal digit (Press X to exit program): ");
+        scanf(" %c", &hexDigit);
 
-
-        if (c =="X"){
+        hexDigit = toupper(hexDigit);
+        
+         if (hexDigit =='X'){
             break;
         }
-    }
 
-    //Logic for translating c (char)
+        //Logic for translating hex digit (char)
+        if ((hexDigit >='0' && hexDigit <='9') || (hexDigit >='A' && hexDigit <='F')){
+            int binaryValue = 0;
+
+            //Convert hex decimal to bin Bitwise/Shifting
+            if(hexDigit >='0' && hexDigit <='9'){
+                binaryValue = hexDigit - '0';
+            }
+            else{
+                binaryValue = hexDigit - 'A' + 10;
+            }
+
     
+    }
 
     return 0;
 }
